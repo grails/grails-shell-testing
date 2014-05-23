0a9879ac0ab2b04d7cbe5080d1a769770a9c5873
Lari Hotari <lhotari@gopivotal.com>
2014-05-23 23:25:19 +0300

    remove -xe mode from script

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -xe
+#!/bin/bash
 rm -rf ~/.gvm
 curl -s get.gvmtool.net | bash
 perl -i -p -e 's/gvm_auto_answer=false/gvm_auto_answer=true/' ~/.gvm/etc/config
 source ~/.gvm/bin/gvm-init.sh
 gvm install grails
 gvm use grails
+set +xe
 ./run_reloading_tests.sh
