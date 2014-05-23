f0be1af94cdbb397cdecee9f6602da7ffef8cf88
Lari Hotari <lhotari@gopivotal.com>
2014-05-23 23:20:24 +0300

    remove travis ~/.gvm directory (related to go-lang)

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
+rm -rf ~/.gvm
 curl -s get.gvmtool.net | bash
 perl -i -p -e 's/gvm_auto_answer=false/gvm_auto_answer=true/' ~/.gvm/etc/config
 source ~/.gvm/bin/gvm-init.sh
 gvm install grails
+gvm use grails
 ./run_reloading_tests.sh
