f96d89a889c5bb686718c9f694a7fda3b43deb8d
Lari Hotari <lhotari@gopivotal.com>
2014-05-26 12:11:36 +0300

    better way to reproduce bug GRAILS-11430

--- scripts/test_scaffolding_reloading.tcl
+++ scripts/test_scaffolding_reloading.tcl
@@ -12,6 +12,9 @@ send "cd scaffoldingapp\r"
 expect "$ "
 send "grails $argv -plain-output\r"
 wait_grails_prompt
+send "run-app\r"
+wait_grails_prompt
+
 send "create-domain-class Book\r"
 expect "|Created file test/unit/scaffoldingapp/BookSpec.groovy"
 sleep 1
@@ -22,16 +25,16 @@ sleep 1
 expect_exit_on_error "\n"
 addfield "scaffoldingapp/grails-app/domain/scaffoldingapp/Book.groovy" "title"
 
-send "run-app\r"
-wait_grails_prompt
-
 set timeout 10
 expect_exit_on_error "\n"
+
 for {set i 0} {$i < 20} {incr i} {
 	send "!curl http://localhost:8080/scaffoldingapp/book/create\r"
+	exec curl "http://localhost:8080/scaffoldingapp/book/create" &
 	wait_grails_prompt
 	sleep 1
 }
+
 send "exit\r"
 expect "$ "
 exit
